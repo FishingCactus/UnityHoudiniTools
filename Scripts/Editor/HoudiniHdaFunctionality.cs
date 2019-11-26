cd9f6801ef33386cafb687c7946176548bda7343
Michael Delva <michael.delva@fishingcactus.com>
2019-11-26 09:47:59 +0100

    Reformated code

--- Scripts/Editor/HoudiniHdaFunctionality.cs
+++ Scripts/Editor/HoudiniHdaFunctionality.cs
@@ -2,38 +2,36 @@ using UnityEditor;
 using UnityEngine;
 using FishingCactus;
 
-public class HoudiniHdaFunctionality {
+public class HoudiniHdaFunctionality 
+{
+    [MenuItem( "FishingCactus/HoudiniHdaDuplicate" )]
+    public static void onHoudiniHDADuplicate()
+    {
+        GameObject[] root_gameobject = Selection.gameObjects;
 
-	[MenuItem( "FishingCactus/HoudiniHdaDuplicate" )]
+        if( root_gameobject == null )
+        {
+            Debug.LogWarning("Nothing selected. You need to select an asset!");
+            return;
+        }
 
-	public static void onHoudiniHDADuplicate()
-	{
-		GameObject[] rootGO = Selection.gameObjects;
-		GameObject[] NewAsset = new GameObject[rootGO.Length];
-
-		if(rootGO == null)
-		{
-			Debug.LogWarning("Nothing selected. You need to select an asset!");
-			return;
-		}
-		
-		if (rootGO != null )
-		{
-			for( int i = 0; i < rootGO.Length; i += 1 )
+        GameObject[] new_game_object = new GameObject[ root_gameobject.Length ];        
+        for( int i = 0; i < root_gameobject.Length; i += 1 )
+        {
+            var go = root_gameobject[i];
+    
+            // The selected gameobject is the asset's root gameobject.
+            // Get the root component (HEU_HoudiniAssetRoot) which will allow us to get the asset component (HEU_HoudiniAsset) itself.
+            HoudiniEngineUnity.HEU_HoudiniAssetRoot root = go.GetComponent<HoudiniEngineUnity.HEU_HoudiniAssetRoot>();
+            
+            if (root != null )
             {
-				var go = rootGO[i];
-     
-				// The selected gameobject is the asset's root gameobject.
-				// Get the root component (HEU_HoudiniAssetRoot) which will allow us to get the asset component (HEU_HoudiniAsset) itself.
-				HoudiniEngineUnity.HEU_HoudiniAssetRoot root = go.GetComponent<HoudiniEngineUnity.HEU_HoudiniAssetRoot>();
-				if (root != null )
-				{
-					HoudiniEngineUnity.HEU_HoudiniAsset asset = root._houdiniAsset;
-					// This will return a duplicated asset as a gameobject
-					NewAsset[i] = asset.DuplicateAsset();
-				}
-			}
-		}
-		Selection.objects = NewAsset;
-	}
+                HoudiniEngineUnity.HEU_HoudiniAsset asset = root._houdiniAsset;
+                // This will return a duplicated asset as a gameobject
+                new_game_object[i] = asset.DuplicateAsset();
+            }
+        }
+
+        Selection.objects = new_game_object;
+    }
 }
